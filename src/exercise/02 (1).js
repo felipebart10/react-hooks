97146727e8e7ed32463896c95576013fb381bef6
felipebart10 <felipe.bartocci@fatec.sp.gov.br>
2021-03-24 01:24:59 +0000

    (2021-03-23) Exercício 3 e 4

--- src/exercise/02 (1).js
+++ src/exercise/02 (1).js
@@ -7,34 +7,34 @@ function Greeting({initialName = ''}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') || initialName
 
-  // LAZY INITIALIZER: inicializador preguiçoso
-  // Quando o useState recebe uma função ao invés de un valor como estado inicial,
+  // LAZY INIITIALIZER: inicializador "preguiçoso"
+  // Quando o useState recebe uma função em vez de um valor como estado inicial,
   // essa função é executada apenas durante a fase mount do componente, sem se
   // repetir na fase update.
   const [name, setName] = React.useState(() => window.localStorage.getItem('name') || initialName)
   const [count, setCount] = React.useState(0)
   const [nameUC, setNameUC] = React.useState(() => window.localStorage.getItem('nameUC') || initialName)
 
-
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
 
   function handleChange(event) {
-    setName(event.target.value)   
+    setName(event.target.value)
   }
 
   function handleClick(event) {
-      setNameUC(event.target.value.toUpperCase())
+    setNameUC(event.target.value.toUpperCase()) 
   }
 
-  // Efeito colateral a ser executado após o componente ter sido atualizado
+  // Efeito colateral a ser executado após o componente ter sido atulizado
   React.useEffect(() => {
-      // O valor do localStorage será atualizado após a atualização do componente.
-      window.localStorage.setItem('name', name)
-      window.localStorage.setItem('nameUC', nameUC)
-      setCount(count+1)  
-  },[name])
+    // O valor do localStorage será atualizado após a atualização do componente
+    window.localStorage.setItem('name', name)
+    window.localStorage.setItem('nameUC', nameUC)
+    setCount(count + 1)
+  }, [name])  // [] é a lista de dependências, ou seja, esse useEffect é para ser chamado
+  // apenas quando a variável name sofrer alteração
 
   return (
     <div>
@@ -43,7 +43,7 @@ function Greeting({initialName = ''}) {
         <input value={name} onChange={handleChange} onClick={handleClick} id="name" />
       </form>
       {name ? <strong>Hello {name}, {nameUC}</strong> : 'Please type your name'}
-  <p>localStorage: {window.localStorage.getItem('name')} - {window.localStorage.getItem('nameUC')}</p>
+      <p>localStorage: {window.localStorage.getItem('name')} - {window.localStorage.getItem('nameUC')}</p>
       <p>Contagem: {count}</p>
     </div>
   )
